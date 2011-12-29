d63ff57f6b5aee7881ed4732bc452831343663f8
Isaac T <isaac.true@gmail.com>
2011-12-29 12:42:45 +0100

    Various changes
    Change-Id: Ied656bfa2a36267f6bdd2854f959c6de4152ef60

--- device_tf101.mk
+++ device_tf101.mk
@@ -40,18 +40,18 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
         persist.sys.usb.config=mtp
 
 PRODUCT_COPY_FILES += \
-    frameworks/base/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_c$
-    frameworks/base/data/etc/android.hardware.location.gps.xml:system/etc/permissions$
-    frameworks/base/data/etc/android.hardware.wifi.xml:system/etc/permissions/android$
-    frameworks/base/data/etc/android.hardware.sensor.light.xml:system/etc/permissions$
-    frameworks/base/data/etc/android.hardware.sensor.barometer.xml:system/etc/permiss$
-    frameworks/base/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permiss$
-    frameworks/base/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/p$
-    frameworks/base/data/etc/android.hardware.camera.front.xml:system/etc/permissions$
-    frameworks/base/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:sys$
-    frameworks/base/data/etc/android.software.sip.voip.xml:system/etc/permissions/and$
-    frameworks/base/data/etc/android.hardware.usb.host.xml:system/etc/permissions/and$
-    frameworks/base/data/etc/android.hardware.usb.accessory.xml:system/etc/permission$
-    packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/per$
+    frameworks/base/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml \
+    frameworks/base/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/base/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/base/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/base/data/etc/android.hardware.sensor.barometer.xml:system/etc/permissions/android.hardware.sensor.barometer.xml \
+    frameworks/base/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
+    frameworks/base/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+    frameworks/base/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+    frameworks/base/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
+    frameworks/base/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+    frameworks/base/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
+    frameworks/base/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+    packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml
 
 
