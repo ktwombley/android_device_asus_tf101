19c0319b99fdfb1cfe35ab608abf8e79620f96f0
Isaac T <isaac.true@gmail.com>
2011-12-29 02:00:47 +0100

    Made various changes to makefiles Attempted to use custom kernel
    Change-Id: I2cdafb36b71e1d32ea1e9f68cb71703e01df0482

--- device_tf101.mk
+++ device_tf101.mk
@@ -1,9 +1,9 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # The gps config appropriate for this device
-$(call inherit-product, device/common/gps/gps_us_supl.mk)
+#$(call inherit-product, device/common/gps/gps_us_supl.mk)
 
-$(call inherit-product-if-exists, vendor/asus/tf101/tf101-vendor.mk)
+$(call inherit-product-if-exists, vendor/asus/tf101/device-vendor.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/asus/tf101/overlay
 
@@ -22,3 +22,11 @@ $(call inherit-product, build/target/product/full.mk)
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_NAME := full_tf101
 PRODUCT_DEVICE := tf101
+
+include frameworks/base/build/tablet-dalvik-heap.mk
+
+PRODUCT_COPY_FILES += \
+    device/asus/tf101/init.ventana.rc:root/init.stingray.rc \
+    device/asus/tf101/ueventd.ventana.rc:root/ueventd.ventana.rc \
+    device/asus/tf101/default.prop:root/default.prop \
+
